9fb9cc9fb3d70c28bfdaf045ac58f18c4c1e32fc
Nicholas Trigger <nst15@duke.edu>
2023-11-06 15:58:34 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nicholastrigger.com',
+  site: 'https://www.nicholastrigger.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
