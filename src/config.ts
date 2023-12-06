561d62544c85ea4463288a8c18748adf88621034
Nicholas Trigger <nst15@duke.edu>
2023-12-06 14:22:11 -0500

    Update site description in config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Nicholas Trigger';
-export const SITE_DESCRIPTION = 'Hi! This is my site description; I do not know what to write here :D';
+export const SITE_DESCRIPTION = 'Nicholas Trigger\'s personal website.';
