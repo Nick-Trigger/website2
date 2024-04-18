010f6d0a4a61530d182b4c441db2984733361cc9
Trig03 <nicktrig124@gmail.com>
2024-04-17 23:04:27 -0400

    colorrs??

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'white-text': '#f2f2f8',
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dracula", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
-	  }
+	}
 }
