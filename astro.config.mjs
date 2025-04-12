828a0212764554619afea64d4b16a6288e87fefa
Nicholas Trigger <nst15@duke.edu>
2025-04-12 03:34:33 -0400

    Tweak sidebar and CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
@@ -13,5 +19,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap(), mdx(), robotsTxt()]
 });
